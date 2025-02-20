8f431f4c41f3aa48c38d268d7553f02cf04ed0c8
aika-l <67886657+aika-l@users.noreply.github.com>
2025-02-19 22:18:42 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,7 +22,7 @@ terraform {
   backend "s3" {
     bucket = "group5-grafana-practice"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
     encrypt = true
   }
 }
